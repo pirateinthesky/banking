ec086e9c51b1964070ddf166b9bb6481fbfd9602
Can Alaca <canalaca@Cans-MacBook-Pro.local>
2025-03-31 13:54:41 +0200

    Updated Sign Up and Sign In pages. Added functionality.

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const SignUp = () => {
+const signUp = () => {
   return (
-    <div>page</div>
+    <section className='flex-center size-full max-sm:px-6'> 
+    <AuthForm type="sign-up"/>
+    </section>
   )
 }
 
-export default SignUp
+export default signUp
